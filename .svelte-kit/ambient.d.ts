a586f3a22a6fafddab9feecb18967aed65565dad
Jonas Johansson <jns.johansson@gmail.com>
2024-04-17 19:15:33 +0200

    x

--- .svelte-kit/ambient.d.ts
+++ .svelte-kit/ambient.d.ts
@@ -26,6 +26,8 @@
  * ```
  */
 declare module '$env/static/private' {
+	export const OPENAI_API_KEY: string;
+	export const ORIGIN: string;
 	export const MANPATH: string;
 	export const TERM_PROGRAM: string;
 	export const NODE: string;
@@ -104,6 +106,8 @@ declare module '$env/static/public' {
  */
 declare module '$env/dynamic/private' {
 	export const env: {
+		OPENAI_API_KEY: string;
+		ORIGIN: string;
 		MANPATH: string;
 		TERM_PROGRAM: string;
 		NODE: string;
