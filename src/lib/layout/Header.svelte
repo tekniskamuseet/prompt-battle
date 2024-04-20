2c4c008520908ed6c1d9c5f875c20eb1154b4593
Jonas Johansson <jns.johansson@gmail.com>
2024-04-20 06:55:59 +0200

    Annies ändringar

--- src/lib/layout/Header.svelte
+++ src/lib/layout/Header.svelte
@@ -1,31 +1,16 @@
 <script>
 	import { theme, colorizedBackground } from '$lib/store';
-	import img from '$lib/assets/tm-logo.png';
-
+	// import img from '$lib/assets/tm-logo.png';
 
 	let animate = false;
 </script>
 
 <header class="absolute inset-x-0 top-0 flex items-center justify-center py-3">
-	<img src="{img}">
-	<!--<a href="/">
+	<a href="/">
 		<h1
-			class="group font-redaction-50 text-base"
-			on:mouseenter={() => {
-				animate = true;
-			}}
-			on:mouseleave={() => {
-				animate = false;
-			}}
-			class:animate-unpixelate-font-50-0={animate}
-			class:animate-pixelate-font-0-50={!animate}
-			class:text-prompt-orange={$theme === 'orange' &&
-				$colorizedBackground === false}
-			class:text-prompt-blue={$theme === 'blue' &&
-				$colorizedBackground === false}
-			class:text-slate-900={$colorizedBackground === true}
+			class="group text-base" 
 		>
-			Kulturnatten Prompt Battle
+			KULTURNATTEN PROMPT BATTLE
 		</h1>
-	</a>-->
+	</a>
 </header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 	import ThemeSwitch from '$lib/components/ThemeSwitch.svelte';
 	import ActionBar from '$lib/layout/ActionBar.svelte';
 	import Button from '$lib/components/Button.svelte';
-
+	import img from '$lib/assets/tm-logo.png';
 	$colorizedBackground = false;
 </script>
 
@@ -13,6 +13,7 @@
 			Användarnamn:
 			<input
 				bind:value={$user}
+				placeholder="Skriv in ditt användarnamn"
 				name="user-name"
 				type="text"
 				class="mt-5 w-full border-0 border-b border-slate-900 font-redaction text-8xl text-slate-900 focus:outline-none focus:ring-0
@@ -28,6 +29,7 @@
 		<div class="my-4 ml-4">
 			<ThemeSwitch />
 		</div>
+		<img src="{img}" class="max-w-xs">
 		<div class="ml-auto">
 			<Button label="Starta battle" href="/prompt" />
 		</div>
